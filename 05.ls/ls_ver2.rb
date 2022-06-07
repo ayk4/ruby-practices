5d4c396f10b0cb8ce93307fc8ec082564829f588
github ayk4 <fuzi_asahi_6699i@yahoo.co.jp>
2022-06-07 14:58:13 +0200

    マルチバイト対応削除

--- 05.ls/ls_ver2.rb
+++ 05.ls/ls_ver2.rb
@@ -39,18 +39,10 @@ def make_array(current_dir)
   end
 end
 
-class String
-  def mb_ljust(width, padding=' ')
-    output_width = each_char.map{|c| c.bytesize == 1 ? 1 : 2}.reduce(0, &:+)
-    padding_size = [0, width - output_width].max
-    self + padding * padding_size
-  end
-end
-
 def show_files(arrays)
   arrays.transpose.each do |two_dimensional_array|
     two_dimensional_array.each do |file|
-      print "#{file}".mb_ljust(25,' ')
+      print "#{file}".ljust(25,' ')
     end
     print  "\n"
   end
