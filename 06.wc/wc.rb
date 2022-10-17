d4ea689f23005c79cb137976e3cf76d651f89d3a
github ayk4 <fuzi_asahi_6699i@yahoo.co.jp>
2022-10-17 17:31:19 +0200

    wcコマンドの課題提出です

---
+++ 06.wc/wc.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+def main
+  print_result(**option_parse)
+end
+
+def print_result(**options)
+  file_name = ARGV[0]
+  text = IO.read(file_name)
+
+  print_newline_counts = newline_counts(text) if options[:l]
+  print_word_count = word_counts(text) if options[:w]
+  print_byte_count = byte_count(text) if options[:c]
+  print_file_name = file_name if options[:l] || options[:w] || options[:c]
+
+  if options.empty?
+    puts "#{newline_counts(text)} #{word_counts(text)} #{byte_count(text)} #{file_name} "
+  else
+    print "#{print_newline_counts} #{print_word_count} #{print_byte_count} #{print_file_name} \n"
+  end
+end
+
+def option_parse
+  options = {}
+  OptionParser.new do |opt|
+    opt.on('-l', '--lines', 'print the newline counts') { |v| options[:l] = v }
+    opt.on('-w', '--words', 'print the word counts') { |v| options[:w] = v }
+    opt.on('-c', '--bytes', 'print the byte counts') { |v| options[:c] = v }
+    opt.parse!(ARGV)
+  end
+  options
+end
+
+def newline_counts(text)
+  text.count("\n").to_s.rjust(7)
+end
+
+def word_counts(text)
+  text.split.count.to_s.rjust(7)
+end
+
+def byte_count(text)
+  text.bytesize.to_s.rjust(7)
+end
+
+main
