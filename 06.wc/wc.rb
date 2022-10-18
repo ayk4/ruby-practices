e77da430db8986b1a243342f3fb273ebff115491
github ayk4 <fuzi_asahi_6699i@yahoo.co.jp>
2022-10-18 12:03:10 +0200

    コードを一部簡略化

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -13,9 +13,8 @@ def print_result(**options)
   print_newline_counts = newline_counts(text) if options[:l] || options.empty?
   print_word_count = word_counts(text) if options[:w] || options.empty?
   print_byte_count = byte_count(text) if options[:c] || options.empty?
-  print_file_name = file_name unless options.empty?
 
-  print "#{print_newline_counts} #{print_word_count} #{print_byte_count} #{print_file_name} \n"
+  print "#{print_newline_counts} #{print_word_count} #{print_byte_count} #{file_name} \n"
 end
 
 def option_parse
@@ -38,7 +37,7 @@ def word_counts(text)
 end
 
 def byte_count(text)
-  text.bytesize.to_s.rjust(7)
+  text.size.to_s.rjust(7)
 end
 
 main
